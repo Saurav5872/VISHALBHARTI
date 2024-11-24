bee8a7dbe9cf79bad975959f08f4ccea2fc3997f
Saurav5872 <sauravjoshi587296@gmail.com>
2024-11-24 20:15:19 +0530

    Add files via upload

---
+++ VishalBharti/script.js
@@ -0,0 +1,10 @@
+// JavaScript for Chatbot
+const chatbotBtn = document.getElementById('chatbot-btn');
+const chatbotForm = document.getElementById('chatbot-form');
+
+chatbotBtn.addEventListener('click', () => {
+  chatbotForm.classList.toggle('hidden');
+});
+
+
+ 
